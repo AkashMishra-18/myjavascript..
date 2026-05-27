7d8d2ae81b211fdfefa0e47767bd67e4f5630805
Akash Mishra <mishraakash33672@gmail.com>
2026-05-27 03:59:45 +0000

    datatype

---
+++ 01basics/02datatype.js
@@ -0,0 +1,22 @@
+"use strict";//treat all js code as newer version
+//alert(3+3) we are using node js not browser
+let name ="Akash";
+let age = 18;
+let isLoggedIn = "false"
+let state;
+// number =>2 to power 53
+// bigint
+// string =>""
+//boolen => true/false
+//null => standalone value
+// undefine => 
+// symbol => unique
+
+
+
+
+
+// object
+
+console.log(typeof null);
+console.log (typeof state);
