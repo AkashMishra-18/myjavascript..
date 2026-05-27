f7dc89fb8cc09088d08be408888087b3d2a1be30
Akash Mishra <mishraakash33672@gmail.com>
2026-05-27 11:27:13 +0000

    comparison

---
+++ 01basics/04comparison.js
@@ -0,0 +1,9 @@
+// console.log(2>1);
+// console.log(2>!6);
+// console.log(2==7);
+// console.log("2">1);
+// console.log(null==0);
+// console.log(null>0);
+// console.log(null>=0);
+console.log("2"==2);
+console.log("2"===2);//its also check the datatypes
