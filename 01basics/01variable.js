224d908a79f9a5ffab1e343ec62edabf78870908
Akash Mishra <mishraakash33672@gmail.com>
2026-05-27 03:12:48 +0000

    variable concept

---
+++ 01basics/01variable.js
@@ -0,0 +1,7 @@
+const accountId = 2233121
+var accountEmail = "jfhg@gmail.com";
+let accountPassword = 123223
+
+accountcity = 'Allahabad';
+
+console.table([accountId,accountEmail,accountPassword,accountcity]);
