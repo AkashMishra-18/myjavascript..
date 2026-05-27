f10a2f76d0995295a276c5c863af61111e3ef43c
Akash Mishra <mishraakash33672@gmail.com>
2026-05-27 11:09:17 +0000

    conoperations

--- 01basics/03conversionoperation.js
+++ 01basics/03conversionoperation.js
@@ -3,3 +3,11 @@ console.log(typeof score);
 console.log(typeof (score));
 let valueInnumber = Number(score)
 console.log(valueInnumber);
+
+
+//*****************operations***************
+let value =3
+let negvalue = -3
+console.log(negvalue);
+console.log(++value);
+console.log(value++);
