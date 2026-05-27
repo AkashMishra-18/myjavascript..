2c76707f8eaa320a8524c120f1ee1d641c53a5d0
Akash Mishra <mishraakash33672@gmail.com>
2026-05-27 13:12:56 +0000

    stringmethods

---
+++ 01basics/05string.js
@@ -0,0 +1,10 @@
+const name =  "Akash"
+const repoCount = 50
+//console.log(name + repoCount + "values");
+console.log(`Hello ${name} you have total ${repoCount} repocount`);
+
+const gameName = new String("crick")
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toLocaleUpperCase());
