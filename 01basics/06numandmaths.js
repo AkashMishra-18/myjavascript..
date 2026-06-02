57c465caba2ceca4d13e7c77b55129f8d60451f8
Akash Mishra <mishraakash33672@gmail.com>
2026-06-02 03:39:56 +0000

    mathandnum

---
+++ 01basics/06numandmaths.js
@@ -0,0 +1,32 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(typeof(balance));
+// console.log(balance.toFixed(2));
+// const otherNum = 123.674832
+// console.log(otherNum.toPrecision(3));
+
+// const hundrads = 100000000
+// console.log(hundrads.toLocaleString('en-In'))
+
+
+
+//*************************************MATHS**************************** */
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.7))
+// console.log(Math.ceil(8.1))
+// console.log(Math.floor(8.9))
+
+// console.log(Math.random())
+// console.log((Math.random()*10)+1)
+// console.log(Math.floor(Math.random()*10)+1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
