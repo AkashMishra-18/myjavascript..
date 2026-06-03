0bd5715809bdc386273b86c9f2d0affb1d1383be
Akash Mishra <mishraakash33672@gmail.com>
2026-06-03 16:54:43 +0000

    array2

---
+++ 01basics/02basics/02array.js
@@ -0,0 +1,13 @@
+// const marvel_heros = ["thor", "Ironman", "spiderman"]
+// const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros)
+// //console.log(marvel_heros)
+// const allheros = marvel_heros.concat(dc_heros)
+// console.log(allheros);
+// const anarray = [1,3,[5,6,5],[8,[4,5]]]
+// const realarray = anarray.flat(1);
+// console.log(realarray)
+console.log(Array.isArray("akash"))
+console.log(Array.from("akash"))
+console.log(Array.from({name:"akash"}))
