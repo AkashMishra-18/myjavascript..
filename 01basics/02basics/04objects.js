277fe15acb40ef661e097a63ba73b0f7b32cd312
Akash Mishra <mishraakash33672@gmail.com>
2026-06-04 02:19:57 +0000

    objconcept

---
+++ 01basics/02basics/04objects.js
@@ -0,0 +1,29 @@
+//const tinderuser = new Object ()
+const tinderuser = {}
+
+tinderuser.id = "123abh"
+tinderuser.name ="Akash"
+tinderuser.logedin = false
+//console.log(tinderuser)
+
+const regularuser = {
+    email :"akash@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"akash",
+            lastname:"mishra"
+        }
+    }
+}
+//console.log(regularuser.fullname.userfullname.firstname)
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"j", 4:"m"}
+//const obj3 = {obj1, obj2}
+
+//const obj3 = Object.assign({}, obj1,obj2)
+// const obj3 = {...obj1, ...obj2}
+// console.log(obj3)
+console.log(tinderuser)
+console.log(Object.keys(tinderuser));
+console.log(Object.values(tinderuser));
