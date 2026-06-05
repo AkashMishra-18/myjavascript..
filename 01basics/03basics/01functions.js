5bfde104f902b237d19ea11bac0d47a779e2a439
Akash Mishra <mishraakash33672@gmail.com>
2026-06-05 03:56:53 +0000

    functiondetails

--- 01basics/03basics/01functions.js
+++ 01basics/03basics/01functions.js
@@ -22,13 +22,32 @@ function addTwonum(num1,num2){
 const result = addTwonum(3,5)
 //console.log("result:",result)
 
-function loginUsermessage(username){
-    //if(username===undefined)
-    if(!username){
-        console.log("please enter the username")
-        return
-    }
-    return `${username} just logged in`
+// function loginUsermessage(username){
+//     //if(username===undefined)
+//     if(!username){
+//         console.log("please enter the username")
+//         return
+//     }
+//     return `${username} just logged in`
+//}
+//console.log(loginUsermessage())
+const user ={
+    username:"Akash",
+    price:"1000"
 }
-console.log(loginUsermessage())
 
+function handleobjects(anyobjects){
+    console.log(`username is ${anyobjects.username} and price is${anyobjects.price}`)
+}
+//handleobjects(user)
+// handleobjects( {
+//     username:"dip",
+//     price:78907
+// })
+
+const myarray=[23,100,4000,5454,765]
+
+function returnSecondvalue(getarray){
+    return getarray
+}
+console.log(returnSecondvalue(myarray))
