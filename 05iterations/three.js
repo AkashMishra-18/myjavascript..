8138cf624d6c97c9251b759ef850a9c177c530ec
Akash Mishra <mishraakash33672@gmail.com>
2026-06-25 14:47:26 +0000

    changes

---
+++ 05iterations/three.js
@@ -0,0 +1,18 @@
+// for of
+// ["","", ""]
+// [{},{}, {}]
+
+// const arr = [1,2,3,4,5]
+// for (const num of arr) {
+//     console.log(num);
+    
+// }
+
+//map 
+const map = new Map()
+map.set('IN', "india");
+map.set('USA', "united state of america");
+map.set('USA', "united state of america")
+map.set('USA', "united state of america")
+
+console.log(map)
---
+++ 05iterations/two.js
@@ -0,0 +1,20 @@
+// let index = 0
+// while(index <= 10 ){
+//     console.log(`value of index is ${index}`);
+//     index = index + 2
+// }
+
+// let myarray = ["krish", "batman", "spiderman"]
+
+// let arr = 0
+// while (arr < myarray.length){
+//     console.log (myarray[arr]);
+//     arr ++
+// }
+
+let score = 11
+do {
+    console.log(`score is ${score}`);
+    score++
+    
+} while (score<10);
