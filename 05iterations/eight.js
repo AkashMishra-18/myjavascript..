001855f542e4a41758a183e36c4c83b6f9e9156e
Akash Mishra <mishraakash33672@gmail.com>
2026-06-26 04:49:40 +0000

    reduce method

---
+++ 05iterations/eight.js
@@ -0,0 +1,20 @@
+const mynum =[1,2,3]
+// const mytotal = mynum.reduce(function (accumulator, currentValue) {
+//     console.log(`accumulator: ${accumulator}, currentValue: ${currentValue}`);
+//     return accumulator + currentValue;
+// }, 0)
+
+const mytotal = mynum.reduce((acc , cuuval) => (acc + cuuval),0)
+console.log(mytotal)
+
+const shoppingcart = [
+    {item: "laptop", price: 1000},
+    {item: "phone", price: 500},
+    {item: "tablet", price: 750}
+    ]
+
+const totalshoppingbill  = shoppingcart.reduce((acc, item) => {
+    console.log(`acc: ${acc}, item: ${item.price}`);
+    return acc + item.price;
+}, 0)
+console.log(totalshoppingbill)
