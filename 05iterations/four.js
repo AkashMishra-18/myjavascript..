e14b3af027b7c25aed8020709b94aa73eb647059
Akash Mishra <mishraakash33672@gmail.com>
2026-06-26 03:04:53 +0000

    forof and forin

---
+++ 05iterations/four.js
@@ -0,0 +1,16 @@
+const myobject = {
+    js : "Javascript",
+    cpp: 'c++',
+    py : "python",
+    rb : "ruby"
+    }
+    for (const key in myobject) {
+        // console.log(`${key} is shortcut of ${myobject[key]}`);
+        
+    }
+
+    const programing = ["js", "cpp", "py", "rb"]
+    for (const key in programing) {
+        console.log(`${programing[key]}`);
+        
+    }
